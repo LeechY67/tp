68cb63c60c623e5623909a071893e61c498eeab3
brenda77777 <e1490185@u.nus.edu>
2026-03-11 22:10:33 +0800

    Fix test coverage

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -93,7 +93,6 @@ public class MainWindow extends UiPart<Stage> {
          * ResultDisplay contains a TextArea, thus some accelerators (e.g F1) will
          * not work when the focus is in them because the key event is consumed by
          * the TextInputControl(s).
-         *
          * For now, we add following event filter to capture such key events and open
          * help window purposely so to support accelerators even when focus is
          * in CommandBox or ResultDisplay.
