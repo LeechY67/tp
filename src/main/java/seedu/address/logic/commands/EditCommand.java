94cc98c87708a60d6bff9866b93c10a3ff4b9b17
Jiya-OH <ohjingyang@gmail.com>
2026-03-12 11:01:13 +0800

    Edit EditCommand.java by including Status field

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -26,6 +26,7 @@ import seedu.address.model.application.Company;
 import seedu.address.model.application.HrEmail;
 import seedu.address.model.application.Phone;
 import seedu.address.model.application.Role;
+import seedu.address.model.application.Status;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -101,8 +102,9 @@ public class EditCommand extends Command {
         HrEmail updatedHrEmail = editApplicationDescriptor.getHrEmail().orElse(applicationToEdit.getHrEmail());
         Company updatedCompany = editApplicationDescriptor.getCompany().orElse(applicationToEdit.getCompany());
         Set<Tag> updatedTags = editApplicationDescriptor.getTags().orElse(applicationToEdit.getTags());
+        Status updatedStatus = editApplicationDescriptor.getStatus().orElse(applicationToEdit.getStatus());
 
-        return new Application(updatedRole, updatedPhone, updatedHrEmail, updatedCompany, updatedTags);
+        return new Application(updatedRole, updatedPhone, updatedHrEmail, updatedCompany, updatedTags, updatedStatus);
     }
 
     @Override
@@ -138,6 +140,7 @@ public class EditCommand extends Command {
         private HrEmail hrEmail;
         private Company company;
         private Set<Tag> tags;
+        private Status status;
 
         public EditApplicationDescriptor() {}
 
@@ -151,6 +154,7 @@ public class EditCommand extends Command {
             setHrEmail(toCopy.hrEmail);
             setCompany(toCopy.company);
             setTags(toCopy.tags);
+            setStatus(toCopy.status);
         }
 
         /**
@@ -207,6 +211,18 @@ public class EditCommand extends Command {
          */
         public Optional<Set<Tag>> getTags() {
             return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+        /**
+         * Sets {@code status} to this object's {@code status}.
+         * @param status to be set to the object's status
+         */
+        public void setStatus(Status status) {
+            this.status = status;
+        }
+
+        public Optional<Status> getStatus() {
+            return Optional.ofNullable(status);
         }
 
         @Override
