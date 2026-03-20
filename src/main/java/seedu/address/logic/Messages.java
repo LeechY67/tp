7492cb6b93b8ef2398a492b6ab6fde01996506a2
Jiya-OH <ohjingyang@gmail.com>
2026-03-20 14:07:51 +0800

    Update invalid status message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -19,6 +19,7 @@ public class Messages {
     public static final String MESSAGE_APPLICATIONS_LISTED_OVERVIEW = "%1$d applications listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
             "Multiple values specified for the following single-valued field(s): ";
+    public static final String MESSAGE_INVALID_STATUS = "Invalid status! \n%1$s";
     /**
      * Returns an error message indicating the duplicate prefixes.
      */
